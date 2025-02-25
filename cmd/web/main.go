538d50e5152073652ed5f54d141a67e5aa12ef67
Mickali Garbutt <2020151994@ub.edu.bz>
2025-02-25 10:44:48 -0600

    Working Template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"html/template"
 )
 
 // A handler function named 'home'
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from newsletter"))
+	tmpl, err := template.ParseFiles("ui/html/home.tmpl")
+	if err != nil { 
+     log.Print(err.Error())
+     http.Error(w, "Internal Server Error",http.StatusInternalServerError)   
+     return 
+	}
+	data := map[string]string {
+    "Title": "Tuesday | Thursday",
+    "Greeting": "Good morning!",
+    "Message": "I am having a fantastic day today",
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil { 
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error",http.StatusInternalServerError)   
+		return 
+   }
+   
 }
+
 
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("this the about page"))
