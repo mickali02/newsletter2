f0d07bfae44e2a13aeb6052deb247c1d3c3743f9
Mickali Garbutt <2020151994@ub.edu.bz>
2025-02-25 09:20:21 -0600

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,11 @@
+// Filename:cmd/web/main.go
+// create a basic hello world
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mickali02/newsletter2
+
+go 1.23.5
