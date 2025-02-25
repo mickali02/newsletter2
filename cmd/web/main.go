659e0ba65253f2371d8a0978bcdecd3aa13f7193
Mickali Garbutt <2020151994@ub.edu.bz>
2025-02-25 10:24:44 -0600

    Tests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,26 +2,36 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 // A handler function named 'home'
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello from newsletter"))
+	w.Write([]byte("Hello from newsletter"))
 }
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("this the about page"))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("this the sign up page"))
+}
+
 func main() {
 
-    // mux is our router (multiplexer)
-    mux := http.NewServeMux()
+	// mux is our router (multiplexer)
+	mux := http.NewServeMux()
 
-    // the route pattern/endpoint/URL path
-    mux.HandleFunc("/", home)
+	// the route pattern/endpoint/URL path
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/about", about)
+	mux.HandleFunc("/signup", signup)
 
-    
-    // start a local web server
-    log.Print("starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	// start a local web server
+	log.Print("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 
 }
--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -6,15 +6,59 @@ import (
     "net/http/httptest"
     "testing"
 )
+func TestSignupHandler(t *testing.T) {
+    // set up request and response
+    req := httptest.NewRequest("GET", "/signup", nil)
+    rr := httptest.NewRecorder()
+    // convert the home  function to a http.Handler type
+    handler := http.HandlerFunc(signup)
+    // pass the fake HTTP request to the handler
+    handler.ServeHTTP(rr, req)
+    // perform  the assertions
+    //1. check the status code for 200 OK
+    status := rr.Code
+    if status != http.StatusOK {
+      t.Errorf("got %v, expected %v", status, http.StatusOK)
+    }
+    //2. check the response body
+    expected := "this the sign up page"
+    got := rr.Body.String()
+    if got != expected {
+      t.Errorf("got %v, expected %v", got, expected)
+    }
+}
+
+func TestAboutHandler(t *testing.T) {
+    // set up request and response
+    req := httptest.NewRequest("GET", "/about", nil)
+    rr := httptest.NewRecorder()
+    // convert the home  function to a http.Handler type
+    handler := http.HandlerFunc(about)
+    // pass the fake HTTP request to the handler
+    handler.ServeHTTP(rr, req)
+    // perform  the assertions
+    //1. check the status code for 200 OK
+    status := rr.Code
+    if status != http.StatusOK {
+      t.Errorf("got %v, expected %v", status, http.StatusOK)
+    }
+    //2. check the response body
+    expected := "this the about page"
+    got := rr.Body.String()
+    if got != expected {
+      t.Errorf("got %v, expected %v", got, expected)
+    }
+}
+
 func TestHomeHandler(t *testing.T) {
     // set up request and response
-    req := httptest.NewRequest("GET", "/", nil)
+    req := httptest.NewRequest("GET", "/home", nil)
     rr := httptest.NewRecorder()
     // convert the home  function to a http.Handler type
     handler := http.HandlerFunc(home)
     // pass the fake HTTP request to the handler
     handler.ServeHTTP(rr, req)
-// perform  the assertions
+    // perform  the assertions
     //1. check the status code for 200 OK
     status := rr.Code
     if status != http.StatusOK {
